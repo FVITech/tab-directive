1178faddf45c54747641eefd497e101085045db1
Randy Ferrer <randfer@gmail.com>
2017-01-17 21:41:37 -0500

    Tabs completed

--- app.js
+++ app.js
@@ -4,9 +4,45 @@
     return {
       restrict: 'E',
       transclude: true,
-      template: '<h2>Hello World!</h2> <div role="tabpanel" ng-transclude></div>',
-      scope: { },
-      link: function(scope, elem, attr) {} 
+      template: ' <div role="tabpanel" ng-show="active" ng-transclude></div>',
+      require: '^tabset',
+      scope: {
+        heading: '@'
+      },
+      link: function(scope, elem, attr, tabsetCtrl) {
+        scope.active = false;
+        tabsetCtrl.addTab(scope);
+      }
     }
   })
+  .directive('tabset', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: { },
+      templateUrl: 'tabset.html',
+      bindToController: true,
+      controllerAs: 'tabset',
+      controller: function() {
+        var self = this;
+        self.tabs = [];
+        self.addTab = function addTab(tab) {
+          self.tabs.push(tab);
+
+          if (self.tabs.length === 1) {
+            tab.active = true;
+          }
+        }
+        self.select = function(selectedTab) {
+          angular.forEach(self.tabs, function(tab) {
+            if (tab.active && tab !== selectedTab) {
+              tab.active = false;
+            }
+          });
+
+          selectedTab.active = true;
+        }
+      }
+    }
+  });
 })(window);
